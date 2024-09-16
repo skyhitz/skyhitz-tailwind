f8f06c8ae21f48776ab96cc0237748cd88ce38b3
Alejo Mendoza <alejandro@skyhitzmusic.com>
2024-09-16 11:19:12 -0500

    update endpoint

--- packages/app/config/config.production.ts
+++ packages/app/config/config.production.ts
@@ -2,7 +2,7 @@ import { ConfigInterface } from './config'
 
 export const ProductionConfig: ConfigInterface = {
   APP_URL: 'https://skyhitz.io',
-  GRAPHQL_URL: 'https://api.skyhitz.io/api/graphql',
+  GRAPHQL_URL: 'https://api.skyhitz.io',
   HORIZON_URL: 'https://horizon.stellar.org',
   STELLAR_EXPERT_URL: 'https://stellar.expert/explorer/public',
   ALGOLIA_APP_ID: 'HSVI9OH0KZ',
