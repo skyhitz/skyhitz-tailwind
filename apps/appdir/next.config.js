5f627d50c74c70b2ba697c4094b55629eeae4ee6
Alejo Mendoza <alejandro@skyhitzmusic.com>
2024-09-19 11:15:54 -0500

    disable cache memory size

--- apps/appdir/next.config.js
+++ apps/appdir/next.config.js
@@ -8,6 +8,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  * @type {import('next').NextConfig}
  */
 module.exports = withBundleAnalyzer({
+  cacheMaxMemorySize: 0,
   async redirects() {
     return [
       {
