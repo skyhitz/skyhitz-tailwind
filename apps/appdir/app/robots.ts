f4c00f6b382a5a8330e1702268d777807cc0a88f
Alejo Mendoza <alejandro@skyhitzmusic.com>
2024-10-23 10:20:04 -0500

    prevent duplicate content on subdomain to get crawled

--- apps/appdir/app/robots.ts
+++ apps/appdir/app/robots.ts
@@ -2,10 +2,20 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        disallow: [
+          '/ipfs/',
+          'http://ipfs.skyhitz.io/',
+          'https://ipfs.skyhitz.io/',
+        ],
+      },
+    ],
     sitemap: 'https://skyhitz.io/sitemap.xml',
   }
 }
