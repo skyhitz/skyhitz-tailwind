20835d21e9d1c14dd4fb216108ed0727c4e019d3
Alejo Mendoza <alejandro@skyhitzmusic.com>
2025-02-14 13:52:55 -0500

    refactor top-up page layout to include header and enable scrolling

--- apps/appdir/app/top-up/page.tsx
+++ apps/appdir/app/top-up/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { View } from 'react-native'
+import { View, ScrollView } from 'react-native'
 import CheckoutForm from 'app/ui/payments/checkout-form'
+import { H1 } from 'app/design/typography'
 
-export default function Page() {
+export default function TopUpPage() {
   return (
-    <View className="App mx-auto flex h-screen w-full items-center justify-center">
-      <CheckoutForm />
-    </View>
+    <ScrollView>
+      <View className="App mx-auto flex min-h-screen w-full items-center justify-center">
+        <H1 className="text-center text-lg">Top Up</H1>
+        <CheckoutForm />
+      </View>
+    </ScrollView>
   )
 }
